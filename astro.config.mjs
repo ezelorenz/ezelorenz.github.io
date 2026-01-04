ee42ddd5d56d0efc2d30f88afb208ebfce291e10
Ezequiel Lorenz <103253375+ezelorenz@users.noreply.github.com>
2026-01-04 20:59:42 -0300

    rollback configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ezelorenz.github.io",
-  base: '/ezelorenz.github.io',
+  site: "https://ezelorenz.github.io"
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
