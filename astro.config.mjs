6c17e94751a77d13d30a316f20567177c290185d
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2026-01-12 08:48:16 -0300

    Fix: Forzar npm y corregir imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
 
 export default defineConfig({
   site: "https://ezelorenz.github.io",
